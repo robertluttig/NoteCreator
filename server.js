ba2170c23e149500c9c6d82fb78a3586deb3bd19
Robert Luttig <robert.luttig@gmail.com>
2020-07-13 19:26:23 -0700

    set up serving from html

--- server.js
+++ server.js
@@ -9,21 +9,32 @@ const app = express();
 // for the server.
 const PORT = process.env.PORT || 8080;
 
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// setup up routes to all files in public using the path relative to the
+// public directory.
+app.use(express.static("public"));
+ 
+// app.use("/api", apiRoutes);
+// app.use("/", htmlRoutes);
 
 // Serving from index.html
-app.get("*", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
   });
   
   app.get("/notes", function(req, res) {
-    res.json(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
+
+// app.use('/index.html', express.static(path.join(__dirname, '/index.html')))
+// app.use('/notes.html', express.static(path.join(__dirname, '/notes.html')))
 
   // The below code effectively "starts" our server
 app.listen(PORT, () => {
     console.log("App listening on PORT: " + PORT);
   });
+
